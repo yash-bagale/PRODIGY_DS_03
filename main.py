1d630f990b39673ffc9133653195d2972f3c866f
Yash <bagaleyash20@gmail.com>
2026-02-15 17:47:57 +0530

    comments

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# -----------------------------
+# ----------------------------
 # Import Libraries
-# -----------------------------
+# ----------------------------
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
